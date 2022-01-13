9d258e0bb06a40ddc41e52e382501aec80173766
Diego Pgm <diego.pegramm@gmail.com>
2022-01-13 12:11:11 -0600

    Improvement in the connection with the database and creating databases and tables, also inserting values into tables

--- db.py
+++ db.py
@@ -1,19 +1,51 @@
+from socket import AI_PASSIVE
 import mysql.connector as sql
 
-db = sql.connect(host="localhost", user="diego", password="1234", database="pruebas")
+db = sql.connect(host="localhost", user="diego", password="1234")
 cursor = db.cursor()
 
-def mysql_read(query, cursor):
+def mysql_read(cursor, table):
+    query = 'SELECT * FROM ' + table 
     cursor.execute(query)
     r = cursor.fetchall()
-    return r
+    print('Results: \n', r)
 
-def mysql_modify(db, query, cursor):
+def mysql_modify(db, cursor):
+    query = 'INSERT INTO prueba (name, email) values ("diego", "diego@hotmail.com")'
     cursor.execute(query)
     db.commit()
 
+def create_database(db, cursor):
+    try:
+        query = 'CREATE database pruebas'
+        cursor.execute(query)
+        db.commit()
+        query = 'USE pruebas'
+        cursor.execute(query)
+    except Exception as e:
+        query = 'use pruebas'
+        cursor.execute(query)
 
-result = mysql('SELECT * FROM manager', cursor)
-print(result)
-mysql_modify('INSERT INTO manager values (diego)')
-    
+
+def create_table(db, cursor):
+    table_name = input('What should the table name be: ')
+    try:
+        query = 'create table ' + table_name + '(name varchar(20) not null, email varchar(30) not null)'
+        cursor.execute(query)
+        db.commit()
+    except Exception as e:
+        print('[-] Table already exists')
+        query = 'SHOW tables'
+        cursor.execute(query)
+        r = cursor.fetchall()
+        print('The tables are:',r)
+    return table_name
+
+
+
+
+create_database(db, cursor)
+table = create_table(db, cursor)
+mysql_read(cursor, table)
+mysql_modify(db, cursor)
+mysql_read(cursor, table)
